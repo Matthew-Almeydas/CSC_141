ce7a09449ecfd311ae8d9d5390fc000b334a6ed3
Matthew-Almeydas <malmeydas12@gmail.com>
2026-09-10 13:52:27 -0400

    test

--- Assignments/03_introducing_lists/03_02_greetings.py
+++ Assignments/03_introducing_lists/03_02_greetings.py
@@ -1,1 +1,10 @@
 """
+let's do lists
+"""
+
+names = ['Brooklyn', 'Widel', 'Ciera', 'Brian']
+messages =['Your 40-yard speed is 6.8 seconds', 'You have a 5.2 speed rating
+           'You can run a 40-yard dash in 6 seconds', 
+           'You can do a 4.2 40-yard dash',
+           ]
+print(names[1] + ", " + messages[1])
