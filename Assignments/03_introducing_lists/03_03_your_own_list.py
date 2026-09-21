598dd0902b577136622177662a808fe00a0b55dd
Matthew-Almeydas <malmeydas12@gmail.com>
2026-09-20 20:13:19 -0400

    completion

--- Assignments/03_introducing_lists/03_03_your_own_list.py
+++ Assignments/03_introducing_lists/03_03_your_own_list.py
@@ -1,5 +1,5 @@
 Vehicles = ["Infiniti q50 rs", "Dodge Durango RT", "BMW X3 40i"]
 
-print
-print
-print
+print("I would like to drive a", Vehicles[1], "but it's a gas guzzler.")
+print("I would like to drive a", Vehicles[0], "but it's hard to find in good condition for a good price.")
+print("I would like to drive a", Vehicles[2], "but maintenance will be expensive.")
