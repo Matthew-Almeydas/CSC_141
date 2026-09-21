5e2974a34b5921fde2965271474bbfffa5983bf7
Matthew-Almeydas <malmeydas12@gmail.com>
2026-09-20 20:24:53 -0400

    redone

--- Assignments/03_introducing_lists/03_04_guest_list.py
+++ Assignments/03_introducing_lists/03_04_guest_list.py
@@ -1,6 +1,6 @@
-Artists= ['Scrim', 'Ruby', 'Bones', 'Xavier Wulf', 'Night Lovell']
-print(f"Dear {Celebrities[0]}, I would like to invite you to dinner")
-print(f"Dear {Celebrities[1]}, I would like to invite you to dinner")
-print(f"Dear {Celebrities[2]}, I would like to invite you to dinner")
-print(f"Dear {Celebrities[3]}, I would like to invite you to dinner")
-print(f"Dear {Celebrities[4]}, I would like to invite you to dinner")
+Artists = ['Scrim', 'Ruby', 'Bones', 'Xavier Wulf', 'Night Lovell']
+print(f"Dear {Artists[0]}, I would like to invite you to dinner")
+print(f"Dear {Artists[1]}, I would like to invite you to dinner")
+print(f"Dear {Artists[2]}, I would like to invite you to dinner")
+print(f"Dear {Artists[3]}, I would like to invite you to dinner")
+print(f"Dear {Artists[4]}, I would like to invite you to dinner")
