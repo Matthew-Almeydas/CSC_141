994821c6cf4b04c789cf2ace5dc3c9421e35f17e
Matthew-Almeydas <malmeydas12@gmail.com>
2026-09-20 20:23:53 -0400

    completion

---
+++ Assignments/03_introducing_lists/03_04_guest_list.py
@@ -0,0 +1,6 @@
+Artists= ['Scrim', 'Ruby', 'Bones', 'Xavier Wulf', 'Night Lovell']
+print(f"Dear {Celebrities[0]}, I would like to invite you to dinner")
+print(f"Dear {Celebrities[1]}, I would like to invite you to dinner")
+print(f"Dear {Celebrities[2]}, I would like to invite you to dinner")
+print(f"Dear {Celebrities[3]}, I would like to invite you to dinner")
+print(f"Dear {Celebrities[4]}, I would like to invite you to dinner")
